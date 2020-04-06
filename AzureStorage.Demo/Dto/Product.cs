41529910af78de604ed7b748f835091ba5506c50
Renicius Pagotto <renicius.pagotto@outlook.com>
2020-04-06 20:36:46 -0300

    First commit

--- AzureStorage.Demo/Dto/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzureStorage.Demo.Dto
-{
-    public class Product
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
-    }
-}
